f7d1542005d38ffc6a124b3017d5a3ee1da54816
Mert Demir (Berkut Teknoloji) <extmertde@d-teknoloji.com.tr>
2022-08-19 15:07:48 +0300

    refactor deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:mrtdmrmrt/mrtdmrmrt.github.io.git master
+git push -f git@github.com:mrtdmrmrt/mrtdmrmrt.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:mrtdmrmrt/mrtdmrmrt.github.io.git master:gh-pages
