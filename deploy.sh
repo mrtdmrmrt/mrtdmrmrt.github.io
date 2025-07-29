842183b808463020737bb140cc286f2d052d3820
Mert DEMİR <36696141+mrtdmrmrt@users.noreply.github.com>
2025-07-29 12:52:29 +0000

    sh changed

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:mrtdmrmrt/mrtdmrmrt.github.io.git master:gh-pages
+git push -f git@github.com:mrtdmrmrt/mrtdmrmrt.github.io.git main:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 
 cd -
